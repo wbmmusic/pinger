4d4167db37329bb8c8a7cbe554a661d7e55c72e6
Marece <wbmmusic@gmail.com>
2022-01-09 13:10:31 -0600

    0.0.21

--- public/preload.js
+++ public/preload.js
@@ -1,10 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron');
-const Cryptr = require('cryptr');
-const cryptr = new Cryptr('myTotalySecretKey');
 
 contextBridge.exposeInMainWorld("electron", {
-    ipcRenderer: { ...ipcRenderer },
+    ipcRenderer: {...ipcRenderer },
     receive: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args)),
     removeListener: (channel) => ipcRenderer.removeAllListeners(channel),
-    cryptr: cryptr
 });
